947bf0fe1877c5c3c022cf896c337760c8aded1c
William <wilguidro@gmail.com>
2025-04-12 21:47:09 +0200

    Add Audio Mixer and script to control BGM

---
+++ unity-audio/Assets/Scripts/BGMController.cs
@@ -0,0 +1,38 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class BGMController : MonoBehaviour
+{
+    private static BGMController instance;
+
+    void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject); // Prevent duplicates
+        }
+    }
+
+    void OnEnable()
+    {
+        SceneManager.sceneLoaded += OnSceneLoaded;
+    }
+
+    void OnDisable()
+    {
+        SceneManager.sceneLoaded -= OnSceneLoaded;
+    }
+
+    void OnSceneLoaded(Scene scene, LoadSceneMode mode)
+    {
+        if (scene.name != "MainMenu")
+        {
+            Destroy(gameObject); // Stop the music
+        }
+    }
+}
